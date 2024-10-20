76e481941ff030eb93c2b0552612663a8b0379d5
IvanVitali89 <ertre89@gmail.com>
2024-10-20 11:21:57 +0200

    snack1 finisced

---
+++ Snack1/main.js
@@ -0,0 +1,35 @@
+//Snack 1
+//Creare un array di oggetti:
+//Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+//Stampare a schermo la bici con peso minore.
+
+// ciclo lungo numero di bici, prende primo valore e lo salvo in var, se è più grande del valore dopo rimane in variabile altrimenti lo sostituisce, stampa valore
+
+const bicycle = [{
+    nome: 'bici1',
+    peso: 15
+}, {
+    nome: 'bici2',
+    peso: 16
+}, {
+    nome: 'bici3',
+    peso: 50
+}, {
+    nome: 'bici4',
+    peso: 32
+}, {
+    nome: 'bici5',
+    peso: 25
+}]
+
+let pesoMAggiore = bicycle[0].peso;
+;
+
+
+for (let i = 1; i < bicycle.length; i++) {
+    if ( pesoMAggiore < bicycle[i].peso){
+        pesoMAggiore = bicycle[i].peso
+    }
+}
+
+console.log(pesoMAggiore);
